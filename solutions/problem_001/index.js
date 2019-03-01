91ef7789c37dbefd658f26efa739d8c1cacde2f0
Otilia Veres <otilia.r.veres@gmail.com>
2019-03-01 21:48:38 +0100

    Tested solution problem 1

--- solutions/problem_001/index.js
+++ solutions/problem_001/index.js
@@ -8,7 +8,7 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 module.exports = {
     solution: function(arrayNumbers, k) {
-        if (arrayNumbers === undefined || list == null)
+        if (arrayNumbers === undefined || arrayNumbers == null)
             return false;
         if (typeof k !== 'number')
             return false;
